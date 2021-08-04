2b1f1d5ef9f0f73e6a1e033c8a57df929acfd012
wojbiel833 <wojbiel833@gmail.com>
2021-08-04 18:31:57 +0200

    Add middleware

--- server.js
+++ server.js
@@ -3,24 +3,45 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "/views")));
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/index.html"));
+  res.show("index.html");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/about.html"));
+  res.show("about.html");
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/contact.html"));
+  res.show("contact.html");
 });
 
 app.get("/info", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/info.html"));
+  res.show("info.html");
 });
 
 app.get("/history", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/history.html"));
+  res.show("history.html");
+});
+
+// app.get("/style.css", (req, res) => {
+//   res.show("style.css");
+// });
+
+// app.get("/test.png", (req, res) => {
+//   res.show("test.png");
+// });
+
+app.use((req, res) => {
+  res.status(404).send("404 not found");
 });
 
 app.listen(8000, () => {
