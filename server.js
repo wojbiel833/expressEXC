10b86853b2766c9e2b98b3066db21ba10c42565d
wojbiel833 <wojbiel833@gmail.com>
2021-08-05 20:56:11 +0200

    Add handlebars & first POST

--- server.js
+++ server.js
@@ -1,46 +1,63 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("express-handlebars");
 
 const app = express();
 
+app.engine(
+  "hbs",
+  hbs({ extname: "hbs", layoutsDir: "./layouts", defaultLayout: "mainLayout" })
+);
+app.set("view engine", ".hbs");
+
 // podlaczanie calego folderu z plikami, najczesciej nazwa to public
 app.use(express.static(path.join(__dirname, "/views")));
+// Jeśli chcesz umożliwić obsługę formularzy x-www-form-urlencoded, dodaj middleware express.urlencoded.
+app.use(express.urlencoded({ extended: false }));
+// Jeśli dodatkowo chcesz odbierać dane w formacie JSON (mogą być wysyłane za pomocą form-data), to również express.json:
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
+// app.use(express.json());
+
+// MIDDLEWARE dodajacy metode.show
+// app.use((req, res, next) => {
+//   res.show = (name) => {
+//     res.sendFile(path.join(__dirname, `/views/${name}`));
+//   };
+//   next();
+// });
+
+app.post("/contact/send-message", (req, res) => {
+  res.json(req.body);
 });
 
 app.get("/", (req, res) => {
-  res.show("index.html");
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.show("about.html");
+  res.render("about", { layout: "dark" });
 });
 
 app.get("/contact", (req, res) => {
-  res.show("contact.html");
+  res.render("contact");
 });
 
 app.get("/info", (req, res) => {
-  res.show("info.html");
+  res.render("info");
 });
 
 app.get("/history", (req, res) => {
-  res.show("history.html");
+  res.render("history");
 });
+
+app.use("/user", (req, res) => {
+  res.render("login");
+}); // pokazuje napis login.html
 
 //Zostaw dla Pawla
 app.use("/home", (res, req) => {
   res.show("Hello world!");
 }); // pokazuje blad show i send tak samo
-
-app.use("/user", (req, res) => {
-  res.show("login.html");
-}); // pokazuje napis login.html
 
 // Podlaczanie plikow  kazdy z osobna
 // app.get("/style.css", (req, res) => {
@@ -50,6 +67,10 @@ app.use("/user", (req, res) => {
 // app.get("/test.png", (req, res) => {
 //   res.show("test.png");
 // });
+
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { layout: false, name: req.params.name });
+});
 
 app.use((req, res) => {
   res.status(404).show("404.png");
